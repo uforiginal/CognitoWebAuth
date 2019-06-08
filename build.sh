6c6ddb8143b069ffb0954baf853c4a16d645c154
Paul Berg <paul@paulwberg.com>
2019-06-08 07:31:58 -0600

    Add license header

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x 
- 
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 # Put the cognito pool and keys into variables 
 echo exports.USERPOOLID = \"${c_cognito_user_pool}\"\; > src/cognitoVars.js 
  
