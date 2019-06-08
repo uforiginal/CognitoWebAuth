c99dcabfd996d5038a9f5b90cfd45995db2d9c86
Paul Berg <paul@paulwberg.com>
2019-06-08 07:20:57 -0600

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x 
+ 
+# Put the cognito pool and keys into variables 
+echo exports.USERPOOLID = \"${c_cognito_user_pool}\"\; > src/cognitoVars.js 
+ 
+echo exports.JWKS = $(curl -s https://cognito-idp.`echo ${c_cognito_user_pool} | cut -f 1 -d _`.amazonaws.com/${c_cognito_user_pool}/.well-known/jwks.json)\; >> src/cognitoVars.js 
+ 
+echo exports.REGION = \"`echo ${c_cognito_user_pool} | cut -f 1 -d _`\"\; >> src/cognitoVars.js 
+ 
+echo exports.CLIENTID = \"${c_cognito_client_id}\"\; >> src/cognitoVars.js 
+ 
+echo exports.DOMAIN_PREFIX = \"${c_domain_prefix}\"\; >> src/cognitoVars.js 
+ 
+echo exports.DOMAIN = \"${c_domain}\"\; >> src/cognitoVars.js 
+ 
+echo exports.CLIENT_SECRET = \"${c_client_secret}\"\; >> src/cognitoVars.js 
+ 
+webpack --mode $1 
+ 
