c0863c553ee42c2dd1daae4dde748c3510dc3fa7
Paul Berg <paul@paulwberg.com>
2019-06-08 07:34:41 -0600

    Add License header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 'use strict'; 
- 
+
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/
+
 const crypto = require('crypto'); 
 const jwt = require('jsonwebtoken'); 
 const jwkToPem = require('jwk-to-pem'); 
